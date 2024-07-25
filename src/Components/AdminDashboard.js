f610fc5d224edfdef33b1fdc5a534aa16c6a270c
Emanuel García <144367754+mirraema19@users.noreply.github.com>
2024-07-25 03:33:09 -0600

    Update

--- src/Components/AdminDashboard.js
+++ src/Components/AdminDashboard.js
@@ -11,7 +11,7 @@ const AdminDashboard = () => {
   useEffect(() => {
     const fetchImages = async () => {
       try {
-        const response = await axios.get('http://54.173.247.52/images'); // Ajusta la URL según tu configuración
+        const response = await axios.get('https://54.173.247.52/images'); // Ajusta la URL según tu configuración
         const sortedImages = sortImages(response.data.data, sortOrder);
         setImages(sortedImages); // Ajusta la ruta a los datos según la respuesta de tu API
       } catch (error) {
@@ -43,7 +43,7 @@ const AdminDashboard = () => {
     }).then(async (result) => {
       if (result.isConfirmed) {
         try {
-          await axios.delete(`http://54.173.247.52/images/${imageId}`); // Ajusta la URL según tu configuración
+          await axios.delete(`https://54.173.247.52/images/${imageId}`); // Ajusta la URL según tu configuración
           setImages(images.filter(image => image.id !== imageId)); // Actualiza el estado eliminando la imagen
           Swal.fire(
             'Eliminada!',
